d1b4dc8f0edd07112ad58e370b3f3bb0d908c50e
geetanjalichawla <geetc1919@gmail.com>
2024-01-15 19:37:46 +0530

    fix: pagination are fixed from all the reports

--- src/pages/admin/reports/AllVehicleList.jsx
+++ src/pages/admin/reports/AllVehicleList.jsx
@@ -1,36 +1,17 @@
-import { useState, useEffect } from "react";
-import { BASE_URL } from "../../../main";
-import CustomTable from "../../../components/utils/customTable";
+
+import { useEffect, useState } from "react";
 import axios from "axios";
-import Pagination from "../../../components/utils/Pagination";
+import { FaSearch as SearchIcon } from 'react-icons/fa'; // Assuming FaSearch is the icon you want to use
+import { BASE_URL } from "../../../main";
+
+import CustomTable from "../../../components/utils/CustomTable2";
 
 const AllVehicleList = () => {
-  const [filesData, setFilesData] = useState([]);
+  const [data, setData] = useState({ data: [], totalPages: 0 });
 
-  const apiUrl = `${BASE_URL}/all-vehicle-list?page=`;
-  const token = localStorage.getItem("token");
-
-  const fetchData = async (page = 1) => {
-    try {
-      axios
-        .get(`${apiUrl}${page}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((res) => {
-          const data = res.data;
-          setFilesData(data.vehiclesList);
-        });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [apiUrl]);
-
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  
   const columns = [
     { Header: "Ser. No.", accessor: "_id" },
     { Header: "Regd. No.", accessor: "regNo" },
@@ -40,20 +21,102 @@ const AllVehicleList = () => {
     { Header: "Agreement No.", accessor: "agreementNo" },
     { Header: "Engine No.", accessor: "engineNo" },
     { Header: "Chasis No.", accessor: "chasisNo" },
-    { Header: "Seezer Name", accessor: "seezerId.name" },
-    
+    { Header: "Seezer Name", accessor: "seezerId.name" }, 
   ];
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const urlPage = parseInt(urlSearchParams.get('page')) || 1;
+    const urlSearch = urlSearchParams.get('search') || '';
+
+    setPage(urlPage);
+    setSearch(urlSearch);
+  }, []);
+
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/all-vehicle-list?page=${page}&search=${search}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setData({
+          data: response.data.vehiclesList,
+          totalPages: response.data.totalPages,
+        });
+
+        const newUrl = `${window.location.pathname}?page=${page}${search ? `&search=${search}`: ""}`;
+        window.history.pushState({ path: newUrl }, '', newUrl);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+          data: [],
+          totalPages: 0,
+        };
+      }
+    };
+
+    fetchDataFromAPI();
+  }, [search, page, token]);
+
 
   return (
-    <div className="container bg-blue-400 text-white  mx-auto my-4">
-      <h2 className="text-2xl font-bold mb-4">All Vehicle Details</h2>
-      <CustomTable
-        columns={columns}
-        data={filesData}
-        searchEnabled={true}
-        filterEnabled={false}
-      />
-      <Pagination />
+    <div className="w-full h-full p-4">
+        <div className="w-full sm:w-[70%] md:w-[50%]">
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="text-gray-300" />
+            </span>
+            <input
+              type="text"
+              className="border border-gray-300 w-full py-2 pl-10"
+              placeholder="Search..."
+              value={search || ""}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+      </div>
+
+      <CustomTable columns={columns} data={data.data} searchEnabled={true} />
+
+{/* pagination here */}
+      <div className="flex justify-center items-center space-x-2">
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage(1)}
+          disabled={page === 1}
+        >
+          1
+        </button>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage((prev) => prev - 1)}
+          disabled={page <= 1}
+        >
+          {"<<"}
+        </button>
+        <p>{page}</p>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage((prev) => prev + 1)}
+          disabled={page >= data.totalPages}
+        >
+          {">>"}
+        </button>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage(data.totalPages)}
+          disabled={page === data.totalPages}
+        >
+          {data.totalPages}
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/admin/reports/HoldVehicleList.jsx
+++ src/pages/admin/reports/HoldVehicleList.jsx
@@ -1,110 +1,25 @@
-import { useState, useEffect } from "react";
-import { BASE_URL } from "../../../main";
-import CustomTable from "../../../components/utils/customTable";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Pagination from "../../../components/utils/Pagination";
+import { FaSearch as SearchIcon } from "react-icons/fa"; // Assuming FaSearch is the icon you want to use
+import { BASE_URL } from "../../../main";
+import CustomTable from "../../../components/utils/CustomTable2";
+import { useDispatch } from "react-redux";
+import { setError, setMessage } from "../../../redux/user/userSlice";
 
 const HoldVehicleList = () => {
-  const [filesData, setFilesData] = useState([]);
+  const [data, setData] = useState({ data: [], totalPages: 0 });
 
-  const apiUrl = `${BASE_URL}/hold-vehicle-list?page=`;
-  const token = localStorage.getItem("token");
-
-  const fetchData = async (page = 1) => {
-    try {
-      axios
-        .get(`${apiUrl}${page}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((res) => {
-          const data = res.data;
-          setFilesData(data.vehiclesList);
-        });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const handleRelease = async (data) => {
-    console.log(data);
-    try {
-      const response = await axios.put(
-        `${BASE_URL}/change-vehicle-status/${data.agreementNo}`,
-        { status: "release" },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      console.log("API Response:", response.data);
-      //   toast.success("Status successfully");
-    } catch (error) {
-      console.error("API Request Error:", error);
-
-      if (error.response) {
-        console.error("Server responded with:", error.response.data.error);
-        const errorMessage =
-          error.response.data.error || "An Error occured. Please try Again!";
-        toast.error(errorMessage);
-      } else if (error.request) {
-        console.error("No response received:", error.request);
-        toast.error("Server didn't respond. Please try again!");
-      } else {
-        console.error("Error setting up the request:", error.message);
-        toast.error("Something went wrong :(");
-      }
-    }
-  };
-  const handleRepo = async (data) => {
-    console.log(data);
-    try {
-      const response = await axios.put(
-        `${BASE_URL}/change-vehicle-status/${data.agreementNo}`,
-        { status: "repo" },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      console.log("API Response:", response.data);
-      //   toast.success("Form submitted successfully");
-    } catch (error) {
-      console.error("API Request Error:", error);
-
-      if (error.response) {
-        console.error("Server responded with:", error.response.data.error);
-        const errorMessage =
-          error.response.data.error || "An Error occured. Please try Again!";
-        // toast.error(errorMessage);
-      } else if (error.request) {
-        console.error("No response received:", error.request);
-        // toast.error("Server didn't respond. Please try again!");
-      } else {
-        console.error("Error setting up the request:", error.message);
-        // toast.error("Something went wrong :(");
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [apiUrl]);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
 
   const columns = [
     { Header: "Regd. ID", accessor: "_id" },
     {
       Header: "Status",
-      Cell: ({ row }) => (
-        <div
-          className="px-2 py-1 font-extrabold text-center border border-black rounded-lg"
-        >
-          {row.original.status}
+      accessor: "status",
+      Cell: ({ value }) => (
+        <div className="px-2 py-1 font-extrabold text-center border border-black rounded-lg">
+          {value}
         </div>
       ),
     },
@@ -140,28 +55,168 @@ const HoldVehicleList = () => {
     { Header: "Seezer Name", accessor: "seezerId.name" },
   ];
 
-  return (
-    <div className="container bg-blue-400 text-white  mx-auto my-4">
-      <h2 className="text-2xl font-bold mb-4">Hold Vehicle Details</h2>
-      <CustomTable
-        columns={columns}
-        data={filesData}
-        searchEnabled={true}
-        filterEnabled={false}
-      />
-      <Pagination />
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
 
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const urlPage = parseInt(urlSearchParams.get("page")) || 1;
+    const urlSearch = urlSearchParams.get("search") || "";
+
+    setPage(urlPage);
+    setSearch(urlSearch);
+  }, []);
+
+  const fetchDataFromAPI = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/hold-vehicle-list?page=${page}&search=${search}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setData({
+        data: response.data.vehiclesList,
+        totalPages: response.data.totalPages,
+      });
+
+      const newUrl = `${window.location.pathname}?page=${page}${
+        search ? `&search=${search}` : ""
+      }`;
+      window.history.pushState({ path: newUrl }, "", newUrl);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return {
+        data: [],
+        totalPages: 0,
+      };
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromAPI();
+  }, [search, page, token]);
+
+  const handleRelease = async (data) => {
+    console.log(data);
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/change-vehicle-status/${data._id}`,
+        { status: "release" },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("API Response:", response.data);
+      dispatch(setMessage(response.data.message));
+      fetchDataFromAPI();
+    } catch (error) {
+      console.error("API Request Error:", error);
+
+      if (error.response) {
+        console.error("Server responded with:", error.response.data.error);
+        const errorMessage =
+          error.response.data.error ||
+          error.response.data.message ||
+          "An Error occured. Please try Again!";
+        dispatch(setError(errorMessage));
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+        dispatch(setError("Server didn't respond. Please try again!"));
+      } else {
+        console.error("Error setting up the request:", error.message);
+        dispatch(setError("Something went wrong :("));
+      }
+    }
+  };
+  const handleRepo = async (data) => {
+    console.log(data);
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/change-vehicle-status/${data._id}`,
+        { status: "repo" },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("API Response:", response.data);
+      dispatch(setMessage(response.data.message));
+      fetchDataFromAPI();
+    } catch (error) {
+      if (error.response) {
+        console.error("Server responded with:", error.response.data.error);
+        const errorMessage =
+          error.response.data.error ||
+          error.response.data.message ||
+          "An Error occured. Please try Again!";
+        dispatch(setError(errorMessage));
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+        dispatch(setError("Server didn't respond. Please try again!"));
+      } else {
+        console.error("Error setting up the request:", error.message);
+        dispatch(setError("Something went wrong :("));
+      }
+    }
+  };
+
+  return (
+    <div className="w-full h-full p-4">
+      <div className="w-full sm:w-[70%] md:w-[50%]">
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="text-gray-300" />
+          </span>
+          <input
+            type="text"
+            className="border border-gray-300 w-full py-2 pl-10"
+            placeholder="Search..."
+            value={search || ""}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <CustomTable columns={columns} data={data.data} searchEnabled={true} />
+
+      {/* pagination here */}
+      <div className="flex justify-center items-center space-x-2">
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage(1)}
+          disabled={page === 1}
+        >
+          1
+        </button>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage((prev) => prev - 1)}
+          disabled={page <= 1}
+        >
+          {"<<"}
+        </button>
+        <p>{page}</p>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage((prev) => prev + 1)}
+          disabled={page >= data.totalPages}
+        >
+          {">>"}
+        </button>
+        <button
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          onClick={() => setPage(data.totalPages)}
+          disabled={page === data.totalPages}
+        >
+          {data.totalPages}
+        </button>
+      </div>
     </div>
   );
 };

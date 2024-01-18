e56f4bc3b13c83cf3eae56cd21466e3c041667bd
Ujjwal Goel <ujjwalg211@gmail.com>
2024-01-18 17:21:57 +0530

    feat : added action column and toasts

--- src/pages/admin/repo-agent-approval/RepoAgentApproval.jsx
+++ src/pages/admin/repo-agent-approval/RepoAgentApproval.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 import Pagination from "../../../components/utils/Pagination";
 import CustomTable2 from "../../../components/utils/CustomTable2";
 import { string, object } from "zod";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const newPasswordSchema = string().min(8);
 
@@ -19,21 +20,39 @@ function RepoAgentApproval() {
   const [totalPages, setTotalPages] = useState(1);
   const [search, setSearch] = useState("");
 
-
   const apiUrl = `${BASE_URL}/get-all-repo-agents`;
   const token = localStorage.getItem("token");
 
-  const fetchData = async (currentPage) => {
+  // const fetchData = async (currentPage) => {
+  //   try {
+  //     const response = await axios.get(`${apiUrl}/?page=${currentPage}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     setRepoAgents(response.data.agents);
+  //     setTotalPages(response.data.totalPages);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+  const fetchData = async (currentPage, search) => {
     try {
-      const response = await axios.get(`${apiUrl}/?page=${currentPage}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.get(
+        `${apiUrl}/?page=${currentPage}&search=${search}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       setRepoAgents(response.data.agents);
-      setTotalPages(response.data.totalPages);
     } catch (error) {
       console.error("Error fetching data:", error);
+      return {
+        data: [],
+        totalPages: 0,
+      };
     }
   };
 
@@ -50,21 +69,60 @@ function RepoAgentApproval() {
         }
       );
       console.log("API Response:", response.data);
-      //   toast.success("Status successfully");
+      toast.success(response.data.message);
     } catch (error) {
       console.error("API Request Error:", error);
 
       if (error.response) {
         console.error("Server responded with:", error.response.data.error);
-        // const errorMessage =
-        //   error.response.data.error || "An Error occured. Please try Again!";
-        // toast.error(errorMessage);
+        const errorMessage =
+          error.response.data.error || "An Error occured. Please try Again!";
+        toast.error(errorMessage);
       } else if (error.request) {
         console.error("No response received:", error.request);
-        // toast.error("Server didn't respond. Please try again!");
+        toast.error("Server didn't respond. Please try again!");
       } else {
         console.error("Error setting up the request:", error.message);
-        // toast.error("Something went wrong :(");
+        toast.error("Something went wrong :(");
+      }
+    }
+  };
+
+  const handleAction = async (data) => {
+    console.log(data);
+    let state;
+
+    data.status === "active"
+      ? (state = { status: "inactive" })
+      : (state = { status: "active" });
+
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/change-agent-status/${data._id}`,
+        state,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      fetchData();
+      console.log("API Response:", response.data);
+      toast.success(response.data.message);
+    } catch (error) {
+      console.error("API Request Error:", error);
+
+      if (error.response) {
+        console.error("Server responded with:", error.response.data.error);
+        const errorMessage =
+          error.response.data.error || "An Error occured. Please try Again!";
+        toast.error(errorMessage);
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+        toast.error("Server didn't respond. Please try again!");
+      } else {
+        console.error("Error setting up the request:", error.message);
+        toast.error("Something went wrong :(");
       }
     }
   };
@@ -73,10 +131,9 @@ function RepoAgentApproval() {
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
   const handleChangePassword = async () => {
-
     try {
       newPasswordSchema.parse(newPassword);
-      setNewPasswordError(""); 
+      setNewPasswordError("");
     } catch (error) {
       setNewPasswordError("New password must be at least 8 characters.");
       return;
@@ -89,8 +146,6 @@ function RepoAgentApproval() {
       setConfirmPasswordError("");
     }
 
-
-
     try {
       const response = await axios.put(
         `${BASE_URL}/change-agent-password/${selectedAgent._id}`,
@@ -102,22 +157,22 @@ function RepoAgentApproval() {
         }
       );
       console.log("API Response:", response.data);
-      setShowPasswordPopup(false);
-      // toast.success("Status successfully");
+      handlePopupClose();
+      toast.success(response.data.message);
     } catch (error) {
       console.error("API Request Error:", error);
 
       if (error.response) {
         console.error("Server responded with:", error.response.data.error);
-        // const errorMessage =
-        //   error.response.data.error || "An Error occured. Please try Again!";
-        // toast.error(errorMessage);
+        const errorMessage =
+          error.response.data.error || "An Error occured. Please try Again!";
+        toast.error(errorMessage);
       } else if (error.request) {
         console.error("No response received:", error.request);
-        // toast.error("Server didn't respond. Please try again!");
+        toast.error("Server didn't respond. Please try again!");
       } else {
         console.error("Error setting up the request:", error.message);
-        // toast.error("Something went wrong :(");
+        toast.error("Something went wrong :(");
       }
     }
   };
@@ -134,32 +189,31 @@ function RepoAgentApproval() {
   };
 
   useEffect(() => {
-    const fetchData = async (currentPage, search) => {
-      try {
-        const response = await axios.get(
-          `${apiUrl}/?page=${currentPage}&search=${search}`,
-          {
-            headers:{
-              Authorization: `Bearer ${token}`
-            }
-          }
-        );
-        setRepoAgents(response.data.agents);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        return {
-          data: [],
-          totalPages: 0,
-        };
-      }
-    };
+    // const fetchData = async (currentPage, search) => {
+    //   try {
+    //     const response = await axios.get(
+    //       `${apiUrl}/?page=${currentPage}&search=${search}`,
+    //       {
+    //         headers:{
+    //           Authorization: `Bearer ${token}`
+    //         }
+    //       }
+    //     );
+    //     setRepoAgents(response.data.agents);
+    //   } catch (error) {
+    //     console.error("Error fetching data:", error);
+    //     return {
+    //       data: [],
+    //       totalPages: 0,
+    //     };
+    //   }
+    // };
 
     fetchData(currentPage, search);
   }, [search, currentPage]);
 
-
   useEffect(() => {
-    fetchData(currentPage);
+    // fetchData(currentPage);
   }, [apiUrl, currentPage]);
 
   const columns = [
@@ -175,6 +229,22 @@ function RepoAgentApproval() {
         <div className="px-2 py-1 font-extrabold text-center border border-black rounded-lg">
           {row.original.status}
         </div>
+      ),
+    },
+    {
+      Header: "Action",
+      Cell: ({ row }) => (
+        <button
+          onClick={() => handleAction(row.original)}
+          className={`${
+            row.original.status === "active"
+              ? "bg-red-500 hover:bg-red-700"
+              : "bg-green-500 hover:bg-green-700"
+          }
+          p-2 text-white  rounded-lg`}
+        >
+          {row.original.status === "active" ? "Deny" : "Accept"}
+        </button>
       ),
     },
     {
@@ -209,27 +279,30 @@ function RepoAgentApproval() {
       <h2 className="text-2xl font-bold mb-4">Repo Agents Approval</h2>
 
       <div className="w-full h-full p-4">
-      <div className="mb-10">
-        <div className="relative  w-full ">
-          <input
-            type="text"
-            className="border-0.5 p-2 m-2 rounded-xl border-gray-300"
-            placeholder="Search..."
-            value={search || ""}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+        <div className="mb-10">
+          <div className="relative  w-full ">
+            <input
+              type="text"
+              className="border-0.5 p-2 m-2 rounded-xl border-gray-300"
+              placeholder="Search..."
+              value={search || ""}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
         </div>
+
+        <CustomTable2
+          columns={columns}
+          data={repoAgents}
+          filterEnabled={false}
+        />
+
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
       </div>
-
-      <CustomTable2 columns={columns} data={repoAgents} filterEnabled={false} />
-
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-
-    </div>
 
       {showPasswordPopup && selectedAgent && (
         <div className="fixed top-0 left-[35%] max-w-md max-h-md flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
@@ -265,7 +338,9 @@ function RepoAgentApproval() {
               }`}
             />
             {confirmPasswordError && (
-              <p className="text-red-500 text-sm mb-2">{confirmPasswordError}</p>
+              <p className="text-red-500 text-sm mb-2">
+                {confirmPasswordError}
+              </p>
             )}
             <button
               onClick={handleChangePassword}
@@ -276,6 +351,17 @@ function RepoAgentApproval() {
           </div>
         </div>
       )}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }

8d01131e5362943305e75689e22319872d995239
Ujjwal Goel <ujjwalg211@gmail.com>
2024-01-18 12:45:53 +0530

    feat : responsive sidebar menu for mobile screens

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -1,32 +1,14 @@
-// import React from "react";
-// import SidebarItem from "./SidebarItem";
-// import items from "../../Data/sidebar.json";
-
-// const Sidebar = () => {
-//   return (
-//     <div className="sidebar fixed top-[69px] left-0 z-40 w-64 h-screen border-b bg-gray-800 border-gray-700 ">
-//       {items.map((item, index) => (
-//         <SidebarItem key={index} item={item} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
-
-
-import React from "react";
+import React, { useState } from "react";
 import SidebarItem from "./SidebarItem";
 import items from "../../Data/sidebar.json";
-import Logo from "./Logo";
 
-const Sidebar = ({isOpen}) => {
+const Sidebar = ({ isOpen }) => {
   return (
-    <div className="sidebar fixed w-64 h-screen border-b bg-gray-800 border-gray-700 top-0 ">
-      <div className="flex items-center justify-start rtl:justify-end w-full p-2">
-        <Logo />
-      </div>
+    <div
+      className={`${
+        isOpen ? "w-screen transition-all ease-in-out" : "w-0 sm:w-[223px]"
+      } fixed top-[69px] left-0 z-40 text-white h-screen border-b bg-gray-800 border-gray-700 overflow-y-auto transition-all ease-in-out`}
+    >
       {items.map((item, index) => (
         <SidebarItem key={index} item={item} />
       ))}

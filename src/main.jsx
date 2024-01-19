6af1da4dec65a0cdbd890544304bf5ca0337deb1
Ujjwal Goel <ujjwalg211@gmail.com>
2024-01-19 10:29:43 +0530

    fix : URL

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux'
 import {store} from './redux/store.js'
 import './index.css'
 
-export const BASE_URL = "http://13.200.166.86/backend/api/v1";
+export const BASE_URL = "https://13.200.166.86/backend/api/v1";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}> 
+  <Provider store={store}>
     <App/>
 </Provider>
 )
